9777fcc747fc78f6b86cb8798e534d46d31d809b
wisdomMin <1123856742@qq.com>
2017-04-29 09:00:57 +0800

    updates
    巴拉巴拉

---
+++ Angular/Angular.js
@@ -0,0 +1,11 @@
+//npm是nodejs的包管理工具 bower是前端js的包管理工具
+/*
+    1. 先安装bower npm install bower -g
+    2. 安装angular bower install angular#1.6.4
+    3. angular的招牌功能：双向数据绑定 数据库里的数据变化直接体现在视图上，视图上数据的变化数据库里也立马变化 mvvm
+    4. angular的编程模式是mvvm 模型->视图 视图->模型
+    5. mvc 控制器 将相应的数据放到相应的视图里面 路由去实现控制器
+    6. angular里的模板引擎用两个花括号
+
+*/
+
